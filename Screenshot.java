b0f0ee819b4b2750095d0514c3636eb166008694
Hemvarun <varunsampath77@gmail.com>
2025-12-09 15:45:29 +0530

    Add files via upload
    @test

--- Screenshot.java
+++ Screenshot.java
@@ -4,22 +4,31 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 
 
 public class Screenshot {
-
-	public static void main(String[] args) throws IOException {
+@Test
+	public void runScreenshot() throws IOException {
 		//ChromeDriver driver = new ChromeDriver();
 		
 		FirefoxDriver driver = new FirefoxDriver();
 		 
-		driver.get("https://www.flipkart.com/");
+		driver.get("https://leafground.com/alert.xhtml");
 		
 		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//span[text()='show']")).click();
+		
+		Alert simpleAlert = driver.switchTo().alert();
+		
+		simpleAlert.accept();
 		
 		File source = driver.getScreenshotAs(OutputType.FILE);
 		
